ecebfb2d54571d184d15b5c064f2e6ab44b73d1a
StevenHarvey <54606283+StevenHarvey@users.noreply.github.com>
2020-08-01 22:19:10 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='DOXter',
-      version='0.2',
-      description='Test',
-      url='https://github.com/StevenHarvey/DOXter',
-      author='StevenHarvey',
-      author_email=' ',
-      license='MIT',
-      packages=['DOXter'],
-      install_requires=[
-          '<normal_dependency>',
-           '<DOXter> @ git+https://github.com/StevenHarvey/DOXter@master'
-      ],
-      zip_safe=False)
+long_desc = """
+shpgeocode is a library for reverse geocoding using ESRI shapefiles
+"""
+
+
+setup(
+    name='pyshpgeocode',
+    version='0.1',
+    description="reverse geocoding using shapefiles",
+    long_description=long_desc,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    keywords='',
+    author='Gregor Aisch',
+    author_email='gregor.aisch@okfn.org',
+    url='http://okfnlabs.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests','test.*']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=["pyshp"],
+    tests_require=[],
+    entry_points={}
+)
